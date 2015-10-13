128d010a2427300c8b2537155c6444594ab41d64
holeyness <ian@ianluo.com>
2015-10-13 17:41:55 -0400

    Added various variables for the template and removed unnecessary stuffs

--- front-end/routes/index.js
+++ front-end/routes/index.js
@@ -4,13 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var context = {
-        title: "A2Empowerment Dashboard"
+        title: "A2Empowerment Dashboard",
+        pageTitle: "Dashboard",
+        pageSubtitle: "Info at a Glance"
     };
     res.render('main', context);
-});
-
-router.get('/data', function(req, res, next) {
-    var data; //get the data for the page
 });
 
 module.exports = router;
