5ac5953c67ddfda76e23ff0f361cc4c5d4a868e2
Nick Sempere <nick.sempere@sbcglobal.net>
2015-10-17 12:38:23 -0400

    Added login page and corresponding route

---
+++ front-end/routes/login.js
@@ -0,0 +1,6 @@
+/**
+ * Created by nicolassempere on 10/16/15.
+ */
+var express = require('express');
+var router = express.Router();
+
--- front-end/routes/users.js
+++ front-end/routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get("/login", function (req, res) {
+  res.sendfile("../startbootstrap-sb-admin-1.0.3/login.html", {root: __dirname});
+});
+
 module.exports = router;
