4cf62c37df94385c9df8b2d2c4cd671ccb4fe9fa
Rex Umezuruike <rexumezuruike@wr-130-64-195-84.medford.tufts.edu>
2015-10-26 23:35:13 -0400

    Adding graph and data to dashboard

--- front-end/routes/index.js
+++ front-end/routes/index.js
@@ -9,11 +9,45 @@ var backendURL = '/api/get';
 router.get('/', function(req, res, next) {
 
     var donationsToday, donationsThisWeek, amountToday, amountThisWeek = 0;
+    var week = [];
     if (debug) {
         donationsToday = 4;
         donationsThisWeek = 178;
         amountToday = 170;
         amountThisWeek = 1500;
+
+        var day0 = {};
+        var day1 = {};
+        var day2 = {};
+        var day3 = {};
+        var day4 = {};
+        var day5 = {};
+        var day6 = {};
+
+        day0['amount'] = 100;
+        day0['num'] = 10;
+        week.push(day0);
+        day1['amount'] = 150;
+        day1['num']= 2;
+        week.push(day1);
+        day2['amount'] = 20;
+        day2['num'] = 170;
+        week.push(day2);
+        day3['amount'] = 170;
+        day3['num'] = 5;
+        week.push(day3);
+        day4['amount'] = 17;
+        day4['num'] = 1;
+        week.push(day4);
+        day5['amount'] = 0;
+        day5['num'] = 1;
+        week.push(day5);
+        day6['amount'] = 12;
+        day6['num'] = 12;
+        week.push(day6);
+
+
+
     } else {
         request(backendURL, function (error, response, body) {
           if (!error && response.statusCode == 200) {
@@ -23,13 +57,18 @@ router.get('/', function(req, res, next) {
                 if (body.donations[i].date == today) {
                     donationsToday ++;
                     amountToday += body.donations[i].amount;
-
                 }
                 var thisweek = new Date(); //minus seven days
                 if (body.donations[i].date >= thisweek) {
                     donationsThisWeek++;
                     donationsThisWeek += body.donations[i].amount;
                 }
+                // my code follows up until };
+                for (var j = 0; j < 7; j++) {
+                    if (body.donations[i].date == today - j) {
+                        week[j].amount += body.donations[i].date;                    }            
+                        week[j].donations++;
+                };
             };
 
           } else {
@@ -37,6 +76,11 @@ router.get('/', function(req, res, next) {
           }            
         });
     }
+
+    var today = new Date();
+    for (var i = 0; i < week.length; i++) {
+        week[i].day = today - 7 + i;
+    };
 
     var context = {
         layout: 'dashboard',
@@ -46,7 +90,8 @@ router.get('/', function(req, res, next) {
         donationsToday: donationsToday,
         donationsThisWeek: donationsThisWeek,
         amountToday: amountToday,
-        amountThisWeek, amountThisWeek,
+        amountThisWeek: amountThisWeek,
+        week: week,
     };
     res.render('main', context);
 });
